521e57567d74502f225863051677c960038f07d0
Kyle Boucher <kboucher@calpoly.edu>
2012-04-08 21:18:14 -0700

    finished the flower function

--- Chapter-4.py
+++ Chapter-4.py
@@ -74,6 +74,9 @@ kyle = Turtle()
 print kyle
 
 def polygon(t,l,n):
+    """ Draws a polygon with 'n' number of sides and side length 'l' with
+    turtle 't'.
+    """
     t.delay = 0.01
     for i in range(n):
         lt(t,360.0/n)
@@ -97,6 +100,10 @@ print kyle
 # If k < 1, only one line is drawn. If k < 0, no lines appear (n = 0).
 # Higher accuracy takes more time to draw.
 def circle(t,r,k):
+    """ Draws a circle of radius 'r' using turtle 't'. k is the constant
+    (radius)/(polygon side length) and higher k gives a more accurate circle.
+    k >= 1.
+    """
     t.delay = 0.01
     import math
     alpha = 2*math.acos(1/(2.0*(k))) # gives radians
@@ -120,7 +127,7 @@ def circle(t,r,k):
     pd(t)
 
 
-circle(kyle,20,50)
+circle(kyle,20,30)
 circle(kyle,30,10)
 circle(kyle,40,30)
 
@@ -130,41 +137,105 @@ world = TurtleWorld()
 kyle = Turtle()
 print kyle
 
-# The only thing different about this function from circle is that it takes
-# a parameter 'a' 
+#The only thing different about this function from circle() is that it takes
+#a parameter 'a' which is the angle trhough wich an arc that is part of a
+#circle should be drawn
 def arc(t,r,a,k):
+    """ Draw part of a circle, an arc. Turtle 't' draws an arc, moving
+    through an angle 'a' at radius 'r'. k is the constant (radius)/(polygon
+    side length) and higher k gives a more accurate circle. k >= 1.
+    """  
     t.delay = 0.01
     import math
     alpha = 2*math.acos(1/(2.0*(k))) # gives radians
     theta = math.pi-alpha
     n = math.ceil((a*(math.pi/180))/theta)
-    print n
     worktheta = (a*(math.pi/180))/float(n)
     workalpha = math.pi-worktheta
     l = 2*r*math.cos(workalpha/2) # takes radians
     for i in range(int(n)):
         lt(t,worktheta*(180/math.pi)) # takes degrees
         fd(t,l)
-    # this code returns the turtle to its starting position (needs to be
-    # fixed)
-    #backx = 0
-    #for i in range(int(n)):
-    #    backx = backx + (l*math.cos(i*worktheta))
-    #backy = 0
-    #for i in range(int(n)):
-    #    backy = backy + (l*math.sin(i*worktheta))
-    #backr = math.sqrt((backx**2)+(backy**2))
-    #lt(t,(180/math.pi)*(worktheta-((math.pi/2)-math.atan(backx/backy))))
-    #pu(t)
-    #fd(t,backr)
-    #lt(t,(180/math.pi)*((math.pi/2)-math.atan(backx/backy)))
-    #pd(t)
+
+# this code would return the turtle to its starting position (needs to be
+# fixed)
+#backx = 0
+#for i in range(int(n)):
+#    backx = backx + (l*math.cos(i*worktheta))
+#backy = 0
+#for i in range(int(n)):
+#    backy = backy + (l*math.sin(i*worktheta))
+#backr = math.sqrt((backx**2)+(backy**2))
+#lt(t,(180/math.pi)*(worktheta-((math.pi/2)-math.atan(backx/backy))))
+#pu(t)
+#fd(t,backr)
+#lt(t,(180/math.pi)*((math.pi/2)-math.atan(backx/backy)))
+#pd(t)
 
 arc(kyle,50,180,40)
 arc(kyle,30,270,40)
+
+wait_for_user()
+
+# End of Chapter 4 Excercises
+# 1. see my above functions for their doc strings
+# 2. Stack diagram for my circle function
+# __main__ |    |
+# circle   | t > kyle, r > 20, k > 50 |
+#          | alpha > 3.12159 rad      |
+#          | theta > 0.020000 rad     |
+#          | n > 315                  |
+#          | worktheta > 0.01994662 rad |
+#          | workalpha > 3.121646 rad |
+#          | l > 0.39893              |
+
+# 3. 'flower' shapes. I could work on it more to modify the petal fatness.
+
+def flower(t,p,r,k):
+    """ Draws a flower shape of 'p' petals made of arc segments of a circle
+    with radius 'r'. Drawn using turtle 't'. Accuracy of the circle is
+    determined by k. Higher k gives better accuracy, but takes longer to
+    draw. This function uses the 'arc' function defined previously.
+    """
+    import math
+    t.delay = 0.01
+    angle = 180-(360.0/p)
+    arc(t,r,angle,k)
+    lt(t,(180-angle))
+    for i in range(int(p-1)):
+        arc(t,r,2*angle,k)
+        lt(t,(180-angle))
+    arc(t,r,angle,k)
+
+from TurtleWorld import *
+world = TurtleWorld()
+kyle = Turtle()
+print kyle
+
+flower(kyle,3,50,30)
 
 wait_for_user()
 from TurtleWorld import *
 world = TurtleWorld()
 kyle = Turtle()
 print kyle
+
+flower(kyle,5,100,30)
+
+wait_for_user()
+from TurtleWorld import *
+world = TurtleWorld()
+kyle = Turtle()
+print kyle
+
+flower(kyle,8,100,30)
+
+wait_for_user()
+from TurtleWorld import *
+world = TurtleWorld()
+kyle = Turtle()
+print kyle
+
+flower(kyle,12,100,30)
+
+wait_for_user()
